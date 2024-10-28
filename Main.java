e2a67cfa49ac12defe3dc71e5bfe7964aa7fd164
Michal <michal.gasiewicz03@interia.pl>
2024-10-28 12:50:17 +0100

    commit 3

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Pobranie aktualnej daty i godziny
+        LocalDateTime teraz = LocalDateTime.now();
+
+        // Sformatowanie daty i godziny
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String sformatowanaData = teraz.format(format);
+
+        // Wyświetlenie aktualnej daty i godziny
+        System.out.println("Aktualna data i godzina: " + sformatowanaData);
     }
 }
+
